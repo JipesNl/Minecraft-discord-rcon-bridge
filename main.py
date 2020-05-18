e6c11e7d1110adefed33d4e6f4b2931fb501a9a3
JipesNl <j_verkoulen@hotmail.com>
2020-05-18 13:44:07 +0200

    Added 3 new commands to start, stop & kill server.
    stop & kill commands are only usable by admins or owners

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
+#!/usr/bin/env python3
 from __future__ import print_function
 import argparse
 import socket
 import mcrcon
 import discord
 from discord.ext import commands
+from multicraftapi import MulticraftAPI
+
+mc = MulticraftAPI("http://panel.pebblehost.com/api.php",  "user_name",  "api_key")
+
+def response():
+    response = mc("getServerStatus", "server_id")
+    print(response)
+    if response['success'] == True:
+        print("Connected!")
+        return True
+    else:
+        print("Error!")
+        return False
 
 def main(command):
     # Parse arguments
 
     # Connect
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("HOSTNAME", RCON PORT))
+    sock.connect(("server_ip", 8051))
 
     try:
         # Log in
-        result = mcrcon.login(sock, "RCON PASSWORD")
+        result = mcrcon.login(sock, "rcon_password")
         if not result:
             print("Incorrect rcon password")
             return
@@ -40,6 +54,50 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
+#start command
+@client.command()
+async def start_server(ctx):
+    r = response()
+    if r == True:
+        start = mc("startServer", "server_id")
+        if start['success'] == True:
+            await ctx.send("The server has been started")
+        else:
+            print("Error code 2")
+    else:
+        print("Error code 1")
+        await ctx.send(r)
+
+#stop command
+@client.command()
+@commands.has_any_role('VIP', 'MVP', 'MVP+', 'Admin', 'Owner')
+async def stop_server(ctx):
+    r = response()
+    if r == True:
+        start = mc("stopServer", "server_id")
+        if start['success'] == True:
+            await ctx.send("The server has been stopped")
+        else:
+            print("Error code 2")
+    else:
+        print("Error code 1")
+        await ctx.send(r)
+
+#kill command
+@client.command()
+@commands.has_any_role('VIP', 'MVP', 'MVP+', 'Admin', 'Owner')
+async def kill_server(ctx):
+    r = response()
+    if r == True:
+        start = client("killServer", "server_id")
+        if start['success'] == True:
+            await ctx.send("The server has been stopped")
+        else:
+            print("Error code 2")
+    else:
+        print("Error code 1")
+        await ctx.send(r)
+
 #whitelist command
 @client.command()
 @commands.has_any_role('VIP', 'MVP', 'MVP+', 'Admin', 'Owner')
@@ -58,6 +116,7 @@ async def hidden_cmdl0l(ctx, arg):
 
 #clear command
 @client.command()
+@commands.has_any_role('Admin', 'Owner')
 async def clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
 
@@ -70,11 +129,15 @@ async def help(ctx):
     )
 
     embed.set_author(name='Help')
-    embed.add_field(name='$whitelist [user]', value='Whitelists a user on the server!', inline=False)
-
+    embed.add_field(name='$whitelist [user]', value='Whitelists a user on the server!')
+    embed.add_field(name='$ping', value='Pings the bot!')
+    embed.add_field(name='$start_server', value='Starts the server')
+    embed.add_field(name='$stop_server', value='Stops the server, only usable by Admins')
+    embed.add_field(name='$kill_server', value='Kills the server, only usable by Admins')
+    embed.add_field(name='$help', value='Displays this list')
     await ctx.channel.send(embed=embed)
 
-client.run("DISCORD BOT TOKEN")
+client.run("discord_key")
 
 
 
